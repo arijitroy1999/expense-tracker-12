de804f3a429f059f2bc6a7c0bab9877a5a7c219f
Arijit Roy <arijit.roy1999@gmail.com>
2021-09-18 14:12:45 +0530

    Awesome MERN

---
+++ client/build/precache-manifest.31d377fc141d45bb1ccdf70d00306a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b757ccd5249a1e242bf1c189aa10088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e494734f6515d22f067",
+    "url": "/static/css/main.9e30bff9.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e494734f6515d22f067",
+    "url": "/static/js/main.15ca585f.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
